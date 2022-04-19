422d93e3aa4d4046b5233783251cab451e7863da
Jhonattan <jhonattansueoka@gmail.com>
2022-04-19 00:33:33 -0300

    estruturando e estilizando layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,29 @@ $(document).ready(function(){
     })
 
     wrapperDistance()
+
+
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:5,
+        nav:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            },
+            1500:{
+                items:7
+            }
+        }
+    })
+
 })
 
 // coloca padding em elementos sem wrapper para alinhar a grid
